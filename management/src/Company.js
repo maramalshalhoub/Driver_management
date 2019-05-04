c707a16c5a243bab167491c95498923a3873dd6b
maramalshalhoub <maram_alshalhoub@hotmail.com>
2019-05-04 15:20:00 +0300

    Update

--- management/src/Company.js
+++ management/src/Company.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Company = props => (
     <div>
-
+        <form action="/companys" method="POST">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name"/>
+        <label for="logo">logo</label>
+        <input type="text" name="logo" id="logo" />
+        <label for="address">address</label>
+        <input type="text" name="address" id="address" />
+        <label for="city">city</label>
+        <input type="text" name="city" id="city" />
+        <label for="telephone">telephone</label>
+        <input type="text" name="telephone" id="telephone" />
+        </form>
     </div>
 )
 export default Company;
--- management/src/Driver.js
+++ management/src/Driver.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Driver = props => (
     <div>
-
+        <form action="/drivers" method="POST">
+        <h6>Driver Details:</h6>
+        <label for="driverName">DriverName</label>
+        <input type="text" name="driverName" id="driverName" />
+        <label for="driverAge">Driver Age</label>
+        <input type="text" name="driverAge" id="driverAge" />
+        <label for="driverImage">Driver Image</label>
+        <input type="text" name="driverImage" id="driverImage" />
+        {/* <input type="submit" value="Create Driver Collection"> */}
+        </form>
     </div>
 )
 export default Driver;
